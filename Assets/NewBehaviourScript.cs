33709e9cdc5ca4c6e09f311a9da1b2143d9839d0
Quantum Cat <veronika.netch@gmail.com>
2021-03-08 01:57:29 +0200

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //public GameObject obj;
+    public Color sphereColor;
+    //private MeshRenderer myRenderer;
+    private int _x = 0;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //obj = gameObject;
+        Debug.Log("Hello, world!");
+        sphereColor = new Color(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphereColor = new Color(0, 0, (int)(_x/100));
+        _x++;
+        gameObject.GetComponent<Renderer>().material.color = sphereColor;
+        //myRenderer = obj.GetComponent<MeshRenderer>();
+        //myRenderer.enabled = !myRenderer.enabled;
+        //Material newMaterial = new Material(myRenderer.material);
+        //myRenderer.material.color = sphereColor;
+    }
+}

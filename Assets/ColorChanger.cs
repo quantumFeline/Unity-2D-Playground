8f658d9f928a2640c40e482ed5ad02e1bdcd60a8
Quantum Cat <veronika.netch@gmail.com>
2021-03-08 11:36:21 +0200

    You can move!

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ColorChanger : MonoBehaviour
 {
     //public GameObject obj;
     public Color sphereColor;
-    //private MeshRenderer myRenderer;
+    private Renderer _objectRenderer;
     private int _x = 0;
-     
+
     // Start is called before the first frame update
     void Start()
     {
 
+        _objectRenderer = gameObject.GetComponent<Renderer>();
         //obj = gameObject;
         Debug.Log("Hello, world!");
         sphereColor = new Color(0, 0, 0);
@@ -22,9 +23,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gameObject.
         sphereColor = new Color(0, 0, (int)(_x/100));
         _x++;
-        gameObject.GetComponent<Renderer>().material.color = sphereColor;
+        _objectRenderer.material.color = sphereColor;
         //myRenderer = obj.GetComponent<MeshRenderer>();
         //myRenderer.enabled = !myRenderer.enabled;
         //Material newMaterial = new Material(myRenderer.material);
---
+++ Assets/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    private CharacterController _controller;
+    private float playerSpeed = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * (Time.deltaTime * playerSpeed));
+
+        if (move != Vector3.zero) {
+            Debug.Log("Moving");
+            gameObject.transform.forward = move;
+        }
+    }
+}

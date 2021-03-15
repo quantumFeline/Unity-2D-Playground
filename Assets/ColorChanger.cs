6163b11ab89eb4007f645d27a96b3ebf74c31760
Quantum Cat <veronika.netch@gmail.com>
2021-03-15 10:20:53 +0200

    Quick save...

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,7 +9,10 @@ public class ColorChanger : MonoBehaviour
     //public GameObject obj;
     public Color sphereColor;
     private Renderer _objectRenderer;
-    private int _x = 0;
+    private float _x = 0;
+    private float _step = (float)1.0 / 4096;
+    private int _ticker = 0;
+    private bool _reverse = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +21,33 @@ public class ColorChanger : MonoBehaviour
         _objectRenderer = gameObject.GetComponent<Renderer>();
         //obj = gameObject;
         Debug.Log("Hello, world!");
+        Debug.Log(Color.blue.r + " "+ Color.blue.g + " " + Color.blue.b);
+        Debug.Log(_objectRenderer.material.color);
         sphereColor = new Color(0, 0, 0);
+    }
+
+    void UpdateColour()
+    {
+        _ticker++;
+        //if (_ticker % 100 != 0) return;
+        
+        if (_reverse) _x-= _step;
+        else _x+= _step;
+
+        if (_x == 0 || _x >= 1)
+        {
+            _reverse = !_reverse;
+            //Debug.Log("_x: " + _x + " current colour: " + _objectRenderer.material.color);
+        }
+        //Debug.Log(_x + ": _x");
     }
 
     // Update is called once per frame
     void Update()
     {
         //gameObject.
-        sphereColor = new Color(0, 0, (int)(_x/100));
-        _x++;
+        sphereColor = new Color(0, 0, _x);
+        UpdateColour();
         _objectRenderer.material.color = sphereColor;
         //myRenderer = obj.GetComponent<MeshRenderer>();
         //myRenderer.enabled = !myRenderer.enabled;
--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour
 {
-    private CharacterController _controller;
+    //private CharacterController _controller;
     private float playerSpeed = 2.0f;
+    private Collider2D _collider;
+    //private CollidingBehaviour _behaviour;
+    private Vector3 _lastMove;
     
     // Start is called before the first frame update
     void Start()
     {
-        _controller = gameObject.GetComponent<CharacterController>();
+        _collider = gameObject.GetComponent<BoxCollider2D>();
+        //_behaviour = gameObject.GetComponent<CollidingBehaviour>();
+        Debug.Log(_collider);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _controller.Move(move * (Time.deltaTime * playerSpeed));
-
-        if (move != Vector3.zero) {
-            Debug.Log("Moving");
-            gameObject.transform.forward = move;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 moveAdjusted =  move * (Time.deltaTime * playerSpeed);
+        /*if (_behaviour.isCollided) {
+            gameObject.transform.position -= _lastMove;
         }
+        else
+        {*/
+            _lastMove = moveAdjusted;
+            gameObject.transform.position += moveAdjusted;
+        //}
     }
 }
